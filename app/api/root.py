109e9358e0dddf6b1f430b6a8dc07d953f12a8d3
aimanafzal <96443146+aimanafzal1@users.noreply.github.com>
2022-09-24 19:34:06 +0500

    Adding the template

---
+++ app/api/root.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+from fastapi import FastAPI
+
+root_router = APIRouter()
+
+
+@root_router.get("/")
+def read_root():
+    return {"About": "API_Generator API"}
+
+
+@root_router.get("/healthz")
+def get_healthz():
+    """To check the service health"""
+    return {"status": "ok"}
+
+
+app = FastAPI("A Simple Pagination", debug=True)
